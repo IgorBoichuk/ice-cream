d36af1ec95bceaebc97bc9a261d78d2f88599ea8
IgorBoichuk <118106419+IgorBoichuk@users.noreply.github.com>
2022-12-23 10:59:14 +0000

    Deploying to gh-pages from @ IgorBoichuk/ice-cream@259234f11c88a89dc8b14c5899d3d93caf9ead3e 🚀

---
+++ index.6cf62ccf.js
@@ -0,0 +1,2 @@
+new Swiper(".myS",{autoplay:{delay:1e3},loop:!0}),new Swiper(".mySlider",{pagination:{el:".swiper-pagination",clickable:!0},loop:!0});
+//# sourceMappingURL=index.6cf62ccf.js.map
---
+++ index.d61b2396.js
@@ -0,0 +1,2 @@
+new Swiper(".myS",{autoplay:{delay:1e3},loop:!0}),new Swiper(".mySlider",{pagination:{el:".swiper-pagination",clickable:!0},loop:!0});
+//# sourceMappingURL=index.d61b2396.js.map
